ef425d8cbab1fbdd90831d23651392c54bba9466
ASATILLOEV BUNYOD (STEVEN) <bunyod1234.ba@gmail.com>
2025-09-25 00:20:52 +0900

    feat: MongoDB ga ulanish

---
+++ app.js
@@ -0,0 +1,44 @@
+console.log("Web serverni boshlash");
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
+// MongoDB ni chaqirish
+const db = require("./server").db();
+
+
+
+// 1. Kirish code
+app.use(express.static("public"));                // client lar uchun ochiq folder
+app.use(express.json());                          // json formatida kelgan malumotlarni objectga aylantirib beradi
+app.use(express.urlencoded({extended: true}));    // html dan form requiestlarni qabul qiladi
+
+
+// 2. Session
+
+
+// 3. Views code                  npm i ejs -  urnatib olamiz backend ichida frontend yasaymiz
+app.set("views", "views");        // views folder ichidan o'qiydi, ikkinchisi folder nomi
+app.set("view engine", "ejs");    // ejs traditional usulda fronend ni backend da qurish
+
+
+
+// 4. Routing code
+app.get("/author", function(req, res) {                // author.ejs
+  res.render("author", {user: user});
+});
+
+app.get("/", function(req, res) {                // get client dan keladigan surovni qabul qilib oladi va javob qaytaradi
+  res.render("reja");
+});
--- server.js
+++ server.js
@@ -1,48 +1,27 @@
-console.log("Web serverni boshlash");
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();
+
 const http = require("http");    // http core modul
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
-  }
-});
+let db;
+const connectionStrring = "mongodb+srv://MIT_Steven:tVRjVXZzWYNSP5cX@cluster0.f0cys09.mongodb.net/Reja"
 
-// 1. Kirish code
-app.use(express.static("public"));                // client lar uchun ochiq folder
-app.use(express.json());                          // json formatida kelgan malumotlarni objectga aylantirib beradi
-app.use(express.urlencoded({extended: true}));    // html dan form requiestlarni qabul qiladi
-
-
-// 2. Session
-
-
-// 3. Views code                  npm i ejs -  urnatib olamiz backend ichida frontend yasaymiz
-app.set("views", "views");        // views folder ichidan o'qiydi, ikkinchisi folder nomi
-app.set("view engine", "ejs");    // ejs traditional usulda fronend ni backend da qurish
-
-
-
-// 4. Routing code
-app.get("/author", function(req, res) {                // author.ejs
-  res.render("author", {user: user});
-});
-
-app.get("/", function(req, res) {                // get client dan keladigan surovni qabul qilib oladi va javob qaytaradi
-  res.render("reja");
-});
-
-
-
-
-const server = http.createServer(app);
+mongodb.connect(connectionStrring, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}, (err, client) => {
+  if(err) console.log("ERROR on connection MongoDB");
+  else {
+    console.log("MongoDB connection succeed");
+    console.log(client);
+    module.exports = client;
+    const app = require("./app");
+    const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function() {
   console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+});
+
+  }
+
+
 });
